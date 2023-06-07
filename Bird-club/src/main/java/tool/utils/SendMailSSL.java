f15ebad81c51687e6b3d9d1f0055de88cd4e11fb
ducvx <122201412+ducvx@users.noreply.github.com>
2023-06-07 17:25:55 +0700

    mail test

---
+++ Bird-club/src/main/java/tool/utils/SendMailSSL.java
@@ -0,0 +1,9 @@
+package tool.utils;
+
+public class SendMailSSL{    
+ public static void main(String[] args) {    
+     //from,password,to,subject,message  
+     Mailer.send("prj301.pgnb@gmail.com","nqlgrybvvyqjoaxw","mail mọi người muốn gửi tới","hello javatpoint","<strong>How r u?<strong>");  
+     //change from, password and to  
+ }    
+}    
